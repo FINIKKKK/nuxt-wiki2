0633d34863b65de88625e87e3acd4b28af510db4
FINIKKKK <digitallkrash@gmail.com>
2023-06-15 11:11:38 +0600

    init2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+    css: ['@/assets/styles/style.scss'],
+    modules: ['@pinia/nuxt', '@nuxtjs/svg-sprite'],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "~/assets/styles/assets/variables";',
+                },
+            },
+        },
+    },
+    svgSprite: {
+        input: '~/assets/img',
+        output: '~/assets/sprite',
+    },
+    ssr: true,
 })
