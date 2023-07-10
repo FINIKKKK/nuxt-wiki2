26c924c6b6c3f0fdbbd1da850578f9ad9584910b
FINIKKKK <digitallkrash@gmail.com>
2023-07-10 12:41:52 +0600

    add users pt2

--- pages/companies/[company_id]/add_users.vue
+++ pages/companies/[company_id]/add_users.vue
@@ -8,23 +8,45 @@
     />
 
     <!-- Форма -->
-    <form class="form" @submit.prevent="onInviteUsers">
-      <UIInput
-        label="Введите email адреса через запятую или пробел"
-        v-model="emailUsers"
-        class-name="label"
-        :errors="errors['emails']"
-      />
-      <UISelect
-        :options="roles"
-        v-model="selectValue"
-        type="full"
-        class="select"
-      />
-      <button class="btn" :class="requestController.loading[url]">
+    <div class="form">
+      <div class="content">
+        <div class="emails__input">
+          <!-- Поле ввода -->
+          <UIInput
+            label="Введите email адреса через запятую"
+            v-model="emailUsers"
+            :errors="errors['emails']"
+            @keydown.enter="() => onAddEmail(emailUsers)"
+            @input="onSplitAddEmail"
+          />
+          <div
+            class="btn emails__input-btn"
+            @click="() => onAddEmail(emailUsers)"
+          >
+            Добавить
+          </div>
+        </div>
+
+        <!-- Список emails -->
+        <ul class="emails" v-if="emails.length">
+          <li class="email" v-for="(email, index) in emails" :key="index">
+            <p>{{ email }}</p>
+            <svg-icon name="close" @click="() => onRemoveEmail(email)" />
+          </li>
+        </ul>
+
+        <!-- Селект -->
+        <UISelect :options="roles" v-model="selectValue" type="full" />
+      </div>
+
+      <button
+        class="btn"
+        :class="requestController.loading[url]"
+        @click="onInviteUsers"
+      >
         Выслать приглашение
       </button>
-    </form>
+    </div>
   </NuxtLayout>
 </template>
 
@@ -37,33 +59,74 @@ import { useCustomFetch } from '~/hooks/useCustomFetch';
 import { useTeamStore } from '~/stores/TeamContoller';
 import { useRequestStore } from '~/stores/RequestController';
 import { useFormValidation } from '~/hooks/useFormValidation';
-import { AddUsersScheme } from '~/utils/validation';
+import { AddUsersScheme, AddUsersScheme2 } from '~/utils/validation';
 
+/**
+ * Переменные ----------------
+ */
 const url = 'team/employees/add';
 const emailUsers = ref('');
 const selectValue = ref(roles[0]);
 const teamController = useTeamStore();
 const requestController = useRequestStore();
+const emails = ref([]);
+const router = useRouter();
 
+/**
+ * Хуки ----------------
+ */
 const { errors, validateForm } = useFormValidation(); // Для валидации формы
 
+/**
+ * Методы ----------------
+ */
+// Приглагшаем пользователей
 const onInviteUsers = async () => {
   // Данные запроса
   const dto = {
     team_id: teamController.activeTeamId,
     role: selectValue.value.value,
-    emails: emailUsers.value,
+    emails: emails.value,
   };
 
   // Валидируем данные
-  const isValid = await validateForm(dto, AddUsersScheme);
+  const isValid = await validateForm({ emails: emails.value }, AddUsersScheme);
   if (!isValid) return false;
 
+  // Высылаем приглашения
   const { data } = await useCustomFetch(url, {
     body: dto,
     method: 'POST',
   });
-  console.log(data);
+};
+
+// Добавить email в список
+const onAddEmail = async (email: string) => {
+  console.log(email);
+  // Валидируем данные
+  const isValid = await validateForm(
+    { emails: emailUsers.value },
+    AddUsersScheme2,
+  );
+  if (!isValid) return false;
+
+  if (!emails.value.find((obj) => obj === email)) {
+    // Добавить email
+    emails.value.push(email.trim());
+  }
+  emailUsers.value = '';
+};
+
+// Добавить email в список через запятую
+const onSplitAddEmail = (e) => {
+  if (e.target.value.includes(',')) {
+    onAddEmail(emailUsers.value.split(',')[0]);
+  }
+};
+
+// Удалить email из списка
+const onRemoveEmail = (email: string) => {
+  emails.value = emails.value.filter((obj) => obj !== email);
 };
 </script>
 
@@ -71,15 +134,48 @@ const onInviteUsers = async () => {
 <!-- ----------------------------------------------------- -->
 
 <style lang="scss" scoped>
-.form {
-  width: 529px;
-}
-
-.select {
-  margin-bottom: 32px;
-}
-
 .warning {
   margin: 0px -50px 50px;
+}
+
+.emails__input {
+  position: relative;
+  &-btn {
+    cursor: pointer;
+    position: absolute;
+    top: 6px;
+    right: 6px;
+    font-size: 14px;
+    padding: 8px 24px;
+    background-color: $blue;
+    color: $white;
+  }
+}
+
+.emails {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-bottom: 12px;
+  .email {
+    &:not(:last-child) {
+      margin-right: 20px;
+    }
+    background-color: $blue2;
+    display: flex;
+    align-items: center;
+    border-radius: 2px;
+    padding: 10px 15px;
+    margin-bottom: 20px;
+    p {
+      color: $blue;
+      margin-right: 10px;
+    }
+    svg {
+      width: 17px;
+      height: 17px;
+      cursor: pointer;
+    }
+  }
 }
 </style>
--- utils/validation.ts
+++ utils/validation.ts
@@ -98,28 +98,35 @@ export const ArticleScheme = yup.object().shape({
 });
 
 /**
- * Приглашение пользователей
+ * Приглашение пользователей 1
  */
 export const AddUsersScheme = yup.object().shape({
+  emails: yup.array().min(1, 'Добавьте хотя бы одного пользователя'),
+});
+
+/**
+ * Приглашение пользователей 2
+ */
+export const AddUsersScheme2 = yup.object().shape({
   emails: yup
-      .string()
-      .required('Прежде заполните поле')
-      .test(
-          'valid-emails',
-          'Введите правильный адрес электронной почты или несколько адресов через запятую',
-          function (value) {
-            if (!value) {
-              // Поле пустое, ничего не вводили
-              return true;
-            }
-            // Разделяем введенные адреса по запятой
-            const emailList = value.split(',');
-            // Проверяем каждый адрес на валидность
-            const isValid = emailList.every((email) => {
-              const trimmedEmail = email.trim();
-              return yup.string().email().isValidSync(trimmedEmail);
-            });
-            return isValid;
-          },
-      ),
+    .string()
+    .required('Прежде заполните поле')
+    .test(
+      'valid-emails',
+      'Введите правильный адрес электронной почты',
+      function (value) {
+        if (!value) {
+          // Поле пустое, ничего не вводили
+          return true;
+        }
+        // Разделяем введенные адреса по запятой
+        const emailList = value.split(',');
+        // Проверяем каждый адрес на валидность
+        const isValid = emailList.every((email) => {
+          const trimmedEmail = email.trim();
+          return yup.string().email().isValidSync(trimmedEmail);
+        });
+        return isValid;
+      },
+    ),
 });
