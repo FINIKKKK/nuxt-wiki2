7dbdf9cd9f9cd51f97b4924e22f9fea89c631127
FINIKKKK <digitallkrash@gmail.com>
2023-07-14 16:31:00 +0600

    notices page

--- pages/companies/[company_id]/index.vue
+++ pages/companies/[company_id]/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLayout name="main" :title="$t.title" :nav="nav"></NuxtLayout>
+  <NuxtLayout name="main" :title="$t.home.activity" :nav="nav"></NuxtLayout>
 </template>
 
 <!-- ----------------------------------------------------- -->
@@ -12,8 +12,8 @@ import { useTranslate } from '~/hooks/useTranslate';
  * Переменные ----------------
  */
 const route = useRoute();
-const $t = await useTranslate('activity');
-const nav = [{ label: $t.title, link: route.path }];
+const $t = await useTranslate('sidebar');
+const nav = [{ label: $t.home.activity, link: route.path }];
 </script>
 
 <!-- ----------------------------------------------------- -->
--- pages/companies/[company_id]/notices.vue
+++ pages/companies/[company_id]/notices.vue
@@ -1,5 +1,26 @@
 <template>
-  <NuxtLayout name="main" :title="$t.title" :nav="nav"></NuxtLayout>
+  <NuxtLayout name="main" :title="$t.title" :nav="nav">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Пользователь</th>
+          <th>Действие</th>
+          <th>Дата</th>
+          <th>Статус</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="notice in notifications.notifications" class="notice">
+          <th>
+            <User :data="notice.to" class="user" />
+          </th>
+          <th>{{ notice.params.message }}</th>
+          <th>{{ useFormatDate(notice.created_at) }}</th>
+          <th>{{ notice.read_at ? 'Прочитано' : 'Не прочитано' }}</th>
+        </tr>
+      </tbody>
+    </table>
+  </NuxtLayout>
 </template>
 
 <!-- ----------------------------------------------------- -->
@@ -7,16 +28,46 @@
 
 <script lang="ts" setup>
 import { useTranslate } from '~/hooks/useTranslate';
+import { useCustomFetch } from '~/hooks/useCustomFetch';
+import { useTeamStore } from '~/stores/TeamContoller';
+import { TNotifications } from '~/utils/types/notice';
+import { useFormatDate } from '~/hooks/useFormatData';
 
 /**
  * Переменные ----------------
  */
 const route = useRoute();
+const teamController = useTeamStore();
 const $t = await useTranslate('notices');
 const nav = [{ label: $t.title, link: route.path }];
+
+//
+const { data: notifications } = await useCustomFetch<TNotifications>(
+  `account/notifications`,
+  {
+    query: { team_id: teamController.activeTeamId },
+  },
+);
+console.log(notifications);
 </script>
 
 <!-- ----------------------------------------------------- -->
 <!-- ----------------------------------------------------- -->
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.notice {
+  height: 64px;
+  transition: 0.2s;
+  .user {
+    pointer-events: none;
+    padding: 0 !important;
+  }
+  th {
+    padding: 7px 16px;
+    vertical-align: middle;
+  }
+  &:hover {
+    background-color: $bg;
+  }
+}
+</style>
